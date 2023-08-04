2bc9687341319d184bf61093f9f48fa189ac42c6
GaLangXIX <130811494+GaLangXIX@users.noreply.github.com>
2023-08-04 14:30:39 +0700

    Update hitung_ppn.java
    edit nama dan nim

--- UAS_PBO/hitung_ppn.java
+++ UAS_PBO/hitung_ppn.java
@@ -1,8 +1,8 @@
 /**
  *
- * @author Nimas Ayu Anggun Kharisma
+ * @author Galang Setya Adji W
  * Teknik Informatika - univ. Islam Balitar
- * NIM : 22104410051
+ * NIM : 22104410007
  */
 import javax.swing.JOptionPane;
 public class hitung_ppn implements ppn {
